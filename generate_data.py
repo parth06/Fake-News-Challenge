fb1c2c3cd93321303af87562c89dbcf0fe9806cd
Parth Shah <parth.shah0096@gmail.com>
2019-07-19 13:15:58 -0400

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,41 +1,36 @@
-# Creates the train_test_validation csv files
-#Result of the script will be inside the data folder
-
 import pandas as pd
 from utils.dataset import DataSet
 from utils.generate_test_splits import kfold_split,  get_stances_for_folds
 
+get_dataFrame(data):
+    d = list()
+    for stance in data.stances:
+        s = stance['Stance']
+        # Relatedness
+        if s == 'unrelated':
+          a = s
+          b=None
+          c=None
+        else:
+          a = 'related'
+          ## Decision or Discuss
+          if s == 'discuss':
+            b = s
+            c=None
+          else:
+            b = 'ANA'
+            c = s
+
+        d.append([stance['Headline'],data.articles[stance['Body ID']],s,a,b,c])
+        df = pd.DataFrame(d,columns = ['Headline','Body','Stance','Relatedness','Discussion','AgreeNotagree'])
+    return df
+
 #Training Dataset
 train_data = DataSet()
-folds,hold_out = kfold_split(train_data, training = 0.8, n_folds=1)
-fold_stances, hold_out_stances = get_stances_for_folds(train_data,folds,hold_out)
+train_df = get_dataFrame(train_data)
+train_df.to_csv("data/train.csv", encoding='utf-8', index=False)    
 
-#Test Dataset
+#Training Dataset
 test_data = DataSet("competition_test")
-test_d = list()    
-for stance in test_data.stances:
-    test_d.append([stance['Headline'],stance['Body ID']])
-    
-print("Training Size: ",len(fold_stances[0]))
-print("Validation Size: ",len(hold_out_stances))
-print("Test Size: ",len(test_d))
-
-train_df = pd.DataFrame(fold_stances[0])
-validation_df = pd.DataFrame(hold_out_stances)
-test_df = pd.DataFrame(test_d,columns = ["Headline","Body ID"])
-
-print("Training Shape: ",train_df.shape)
-print("Validation Shape: ",validation_df.shape)
-print("Test Shape: ",test_df.shape)
-
-train_df.to_csv("data/train.csv", encoding='utf-8', index=False)
-validation_df.to_csv("data/validation.csv", encoding='utf-8', index=False)
-test_df.to_csv("data/test.csv", encoding='utf-8', index=False)
-
-train_bodyid = pd.read_csv("fnc-1/train_bodies.csv")
-test_bodyid = pd.read_csv("fnc-1/competition_test_bodies.csv")
-body_id = pd.concat([train_bodyid,test_bodyid])
-
-body_id.sort_values(by=['Body ID'],inplace=True)
-body_id.to_csv("data/body_id.csv", encoding='utf-8', index=False)
-body_id.shape
+test_df = get_dataFrame(test_data)
+test_df.to_csv("data/test.csv", encoding='utf-8', index=False)    
